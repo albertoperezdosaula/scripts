bd11076893a201cb334e65b35b19455d0eacc2d3
Alberto Pérez <email.albertoperez@gmail.com>
2022-05-31 11:07:34 +0200

    Create dns-docker-daemon.sh

---
+++ dns-docker-daemon.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+NC='\033[0m' # No Color
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+BLUE='\033[1;34m'
+YELLOW='\033[1;33m'
+PURPLE='\033[0;35m'
+
+if [ ! -d "/etc/docker" ]
+  then
+    printf "${RED}Docker folder in /etc/docker does not exists!${NC}"
+    printf '\n'
+  exit 1
+fi
+  
+read -p "Add DNS to a new daemon.json file: " DNS
+export DNS
+
+sudo bash -c "echo { >> /etc/docker/daemon.json"
+sudo bash -c "echo '  dns': ['${DNS}'] >> /etc/docker/daemon.json"
+sudo bash -c "echo } >> /etc/docker/daemon.json"
+
+service docker restart
